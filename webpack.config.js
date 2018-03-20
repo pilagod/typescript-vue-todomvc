835f7c026a68e94dc39d9537ca164f1879030bef
pilagod <pilagooood@gmail.com>
2018-03-21 01:11:10 +0800

    init webpack config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.vue']
+  },
+  devtool: 'eval-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+        }
+      }
+    ]
+  }
+}
