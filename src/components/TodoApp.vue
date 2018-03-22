e2938d6bb8375c2d544f608b7ce263e62a569702
pilagod <pilagooood@gmail.com>
2018-03-22 19:09:28 +0800

    add todoapp skeleton

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,13 +1,49 @@
 <template lang="pug">
-    main.todoapp {{ message }}
+    section.todoapp 
+      header.header
+        input.new-todo(autofocus)
+
+      section.main
+        input#toggle-all.toggle-all(type='checkbox')
+        label(for='toggle-all') Mark all as complete
+        ul.todo-list
+          li(v-for='(todo, index) in todos')
+            div.view
+              input.toggle(type='checkbox')
+              label {{ todo.title }}
+              button.destroy
+
+      footer.footer
+        span.todo-count 
+          strong 1 
+          span item left
+
+        ul.filters
+          li
+            a(href='/' @click.prevent='') All
+          li
+            a(href='/' @click.prevent='') Active
+          li
+            a(href='/' @click.prevent='') Completed
+
+        button.clear-completed Clear completed
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import Component from "vue-class-component";
+import { Component, Prop } from "vue-property-decorator";
 
 @Component
 export default class TodoApp extends Vue {
+  @Prop() public todos: object[];
+
   public message: string = "Hello World";
 }
 </script>
+
+<style>
+.todoapp {
+  margin-top: 0px;
+}
+</style>
+
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import Vue from 'vue'
 import TodoApp from './components/TodoApp.vue'
 
-new TodoApp().$mount('main')
+new TodoApp({
+  propsData: {
+    todos: [{
+      title: 'Use Vue with TypeScript',
+      completed: false
+    }]
+  }
+}).$mount('main')
