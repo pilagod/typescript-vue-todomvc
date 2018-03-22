973c86b27ff1b2705c4e7a43024742b11c20fc9f
pilagod <pilagooood@gmail.com>
2018-03-22 16:43:57 +0800

    add init ts-styled vue component

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,9 +1,15 @@
 <template>
     <div>
-        Hello World !
+        {{ message }}
     </div>
 </template>
 
 <script lang="ts">
-export default {};
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class TodoApp extends Vue {
+  public message: string = "Hello World";
+}
 </script>
